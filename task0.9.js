bf352a09b1dd6a1a07cfc1c981a4c0e51990e920
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-08-15 23:52:14 +0100

    Update task0.9.js
    removed repeated vowels

--- task0.9.js
+++ task0.9.js
@@ -1,14 +1,20 @@
 function findvowel(word) {
-  var found = "";
+  var found = '';
   var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
- 
+  
   for (var i = 0; i < word.length; i++) {
     if (vowels.includes(word[i])) {
       found += word[i];
-      if (word.length - 1 !== i) found += ',';
+      if (word.length - 1 !== i) found +=  ',';
     }
   }
   return found
+      .split(',') 
+      .filter(function(item,pos,self){
+      return self.indexOf(item) == pos;
+      })
+      .join(',');
+      
 }
 
 console.log(findvowel('slindile'));
