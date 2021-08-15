e4339d76f95a3c544b8e8b25d933a6a9d5e9a1e8
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-08-15 23:55:42 +0100

    Update task0.10.js
    removed repeated letters in a string

--- task0.10.js
+++ task0.10.js
@@ -1,11 +1,17 @@
-function commonletters(str1, str2) {
+ffunction commonletters(str1, str2) {
     var intersection = "";
     var strarr1 = str1.toLowerCase().split('');
     var strarr2 = str2.toLowerCase().split('');
 
     for (let letter of strarr1)
             if (strarr2.includes(letter)) intersection += letter +',';
-    return intersection;
+    return intersection
+    .split(',') 
+    .filter(function(item,pos,self){
+    return self.indexOf(item) == pos;
+     })
+    .join(',')
+    .replace(/,(\s+)?$/, '.');
 }
 
-console.log(commonletters('house', 'computers'))
+console.log(commonletters('houses', 'computers'));
