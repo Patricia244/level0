de4ac48588d151c73268349f14413d66fbd0c8c9
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-08-15 23:40:34 +0100

    Update task.0.7.js
    changed function names.

--- task.0.7.js
+++ task.0.7.js
@@ -1,10 +1,10 @@
-function celsiusToFahreneit (celsius){
+function celsiusToFahrenheit (celsius){
     var x = celsius;
     var fahrenheit = x * 9/5 + 32;
    
     return fahrenheit;
 }
-console.log(Math.floor(convertTemp(100)));
+console.log(Math.floor( celsiusToFahrenheit(100)));
 
 
 //second function
@@ -12,4 +12,4 @@ function fahrenheitTocelsius(fahren){
     y = fahren
     var celsius = (y -32) * 5/9;
     return celsius
-}console.log(Math.floor(convertTemperature(70)));
+}console.log(Math.floor(fahrenheitTocelsius(70)));
