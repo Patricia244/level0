b494f9cc2e594a87505e71360c118315d35d55f0
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-08-17 11:30:33 +0100

    Update task0.8.js

--- task0.8.js
+++ task0.8.js
@@ -1,16 +1,19 @@
 function  numberToTime(number){
-var hours = Math.floor(number / 60); 
-var minutes = Math.floor(number % 60);
-if (hours > 1 & minutes > 1){
-    return `${hours} hours, ${minutes} minutes`
-}else if( hours<2 & minutes<2){
-    return `${hours} hour, ${minutes} minute`
-}else if( hours> 1 & minutes<2){
-     return `${hours} hours, ${minutes} minute`
-}
-else {
-    return `${hours} hour, ${minutes} minutes`
-}
-
-}
-console.log(numberToTime(133));
+    var hours = Math.floor(number / 60); 
+    var minutes = Math.floor(number % 60);
+    if (hours > 1 & minutes > 1 ||hours ==0 || minutes ==0){
+        return `${hours} hours, ${minutes} minutes`
+    } if( hours<2 & minutes<2){
+        return `${hours} hour, ${minutes} minute`
+    }else if( hours> 1 & minutes<2){
+         return `${hours} hours, ${minutes} minute`
+    
+    }
+    else {
+        return `${hours} hour, ${minutes} minutes`
+    }
+    
+    }
+    console.log(numberToTime(133));
+    console.log(numberToTime(60));
+    console.log(numberToTime(30));
