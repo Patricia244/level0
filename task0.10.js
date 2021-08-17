fcf617a5d1c7d62ab1e2d23eece04fd1768a0d48
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-08-17 11:18:59 +0100

    Update task0.10.js

--- task0.10.js
+++ task0.10.js
@@ -1,4 +1,4 @@
-ffunction commonletters(str1, str2) {
+function commonletters(str1, str2) {
     var intersection = "";
     var strarr1 = str1.toLowerCase().split('');
     var strarr2 = str2.toLowerCase().split('');
