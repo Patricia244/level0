e483623f32f5fcc3f490168fd4a9a30d0c3165aa
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-10-08 14:06:58 +0100

    Update task0.9.js

--- task0.9.js
+++ task0.9.js
@@ -1,20 +1,18 @@
-function findvowel(word) {
-  var found = '';
-  var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-  
+function printVowels(word) {
+  var found = "";
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
   for (var i = 0; i < word.length; i++) {
     if (vowels.includes(word[i])) {
       found += word[i];
-      if (word.length - 1 !== i) found +=  ',';
+      if (word.length - 1 !== i) found += ",";
     }
   }
-  return found.toLowerCase()
-      .split(',') 
-      .filter(function(item,pos,self){
+  return found
+    .toLowerCase()
+    .split(",")
+    .filter(function (item, pos, self) {
       return self.indexOf(item) == pos;
-      })
-      .join(',');
-      
+    })
+    .join(",");
 }
-
-console.log(findvowel('Umumzi'));
