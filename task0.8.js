27a5e46babe46c9e1b8889fe3a496cc8c0c0da08
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-10-08 14:03:47 +0100

    Update task0.8.js

--- task0.8.js
+++ task0.8.js
@@ -1,19 +1,15 @@
-function  numberToTime(number){
-    var hours = Math.floor(number / 60); 
-    var minutes = Math.floor(number % 60);
-    if (hours > 1 & minutes > 1 ||hours ==0 || minutes ==0){
-        return `${hours} hours, ${minutes} minutes`
-    } if( hours<2 & minutes<2){
-        return `${hours} hour, ${minutes} minute`
-    }else if( hours> 1 & minutes<2){
-         return `${hours} hours, ${minutes} minute`
-    
-    }
-    else {
-        return `${hours} hour, ${minutes} minutes`
-    }
-    
-    }
-    console.log(numberToTime(133));
-    console.log(numberToTime(60));
-    console.log(numberToTime(30));
+function numberToTime(number) {
+  var hours = Math.floor(number / 60);
+  var minutes = Math.floor(number % 60);
+  if (hours === 0 && minutes === 0) return `${hours} hours, ${minutes} minutes`;
+  if (hours === 1 && minutes === 1) return `${hours} hour, ${minutes} minute`;
+  if (hours > 1 && minutes > 1) return `${hours} hours, ${minutes} minutes`;
+  if (hours > 1 && minutes === 0) return `${hours} hours, ${minutes} minutes`;
+  if (hours === 0 && minutes > 1) return `${hours} hours, ${minutes} minutes`;
+  if (hours > 1 && minutes === 1) return `${hours} hours, ${minutes} minute`;
+  if (hours === 1 && minutes >=0) return `${hours} hour, ${minutes} minutes`;
+  if (hours >= 0 && minutes === 1) return `${hours} hours, ${minutes} minute`;
+}
+console.log(numberToTime(0));
+console.log(numberToTime(30));
+console.log(numberToTime(500));
