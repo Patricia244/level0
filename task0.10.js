ebfb86aca8d8708da46a8aafa6da1a0d86fed2e6
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-10-08 13:51:41 +0100

    Update task0.10.js

--- task0.10.js
+++ task0.10.js
@@ -1,17 +1,17 @@
-function commonletters(str1, str2) {
-    var intersection = "";
-    var strarr1 = str1.toLowerCase().split('');
-    var strarr2 = str2.toLowerCase().split('');
+function commonLetters(str1, str2) {
+  let sameLetters = "";
+  let strarr1 = str1.toLowerCase().split("");
+  let strarr2 = str2.toLowerCase().split("");
 
-    for (let letter of strarr1)
-            if (strarr2.includes(letter)) intersection += letter +',';
-    return intersection
-    .split(',') 
-    .filter(function(item,pos,self){
-    return self.indexOf(item) == pos;
-     })
-    .join(',')
-    .replace(/,(\s+)?$/, '.');
+  for (let letter of strarr1)
+    if (strarr2.includes(letter)) sameLetters += letter + ",";
+  return sameLetters
+    .split(",")
+    .filter(function (item, pos, self) {
+      return self.indexOf(item) == pos;
+    })
+    .join(",")
+    .replace(/,(\s+)?$/, ".");
 }
 
-console.log(commonletters('houses', 'computers'));
+console.log(commonLetters("houses", "computers"));
