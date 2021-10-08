12784515724992d1f0ea5fd86a73192427f52ed2
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-10-08 13:59:02 +0100

    Update task0.5.js

--- task0.5.js
+++ task0.5.js
@@ -1,6 +1,5 @@
-function areaOfTriangle( x ,y,z){
-     let  s = (x + y+z)/2
-     let Area =  Math.sqrt(s*((s-x)*(s-y)*(s-z)));
-        console.log(Area);
-    }
-areaOfTriangle(10,7,11);
+function areaOfTriangle(x, y, z) {
+  let s = (x + y + z) / 2;
+  let area = Math.sqrt(s * ((s - x) * (s - y) * (s - z)));
+  console.log(area);
+}
