150321c4037a5a18b114eb5fde7ff4bc4d613cb5
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-10-08 13:48:52 +0100

    Update task.0.7.js

--- task.0.7.js
+++ task.0.7.js
@@ -1,15 +1,10 @@
-function celsiusToFahrenheit (celsius){
-    var x = celsius;
-    var fahrenheit = x * 9/5 + 32;
-   
-    return fahrenheit;
+function celsiusToFahrenheit(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
 }
-console.log(Math.floor( celsiusToFahrenheit(100)));
-
 
 //second function
-function fahrenheitTocelsius(fahren){
-    y = fahren
-    var celsius = (y -32) * 5/9;
-    return celsius
-}console.log(Math.floor(fahrenheitTocelsius(70)));
+function fahrenheitToCelsius(fahren) {
+  let celsius = ((fahren - 32) * 5) / 9;
+  return celsius;
+}
