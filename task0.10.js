2775d8452747626e5ed14ab75561969998140900
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-10-13 21:58:30 +0100

    Update task0.10.js

--- task0.10.js
+++ task0.10.js
@@ -1,16 +1,17 @@
 function commonLetters(str1, str2) {
   let sameLetters = "";
-  let strarr1 = str1.toLowerCase().split("");
-  let strarr2 = str2.toLowerCase().split("");
+  let strArr1 = str1.toLowerCase().split("");
+  let strArr2 = str2.toLowerCase().split("");
 
-  for (let letter of strarr1)
-    if (strarr2.includes(letter)) sameLetters += letter + ",";
-  return sameLetters
+  for (let letter of strArr1)
+    if (strArr2.includes(letter)) sameLetters += letter + ",";
+  let char = sameLetters
     .split(",")
     .filter(function (item, pos, self) {
       return self.indexOf(item) === pos;
     })
     .join(",")
-    .replace(/,(\s+)?$/, ".");
+    .replace(/,(\s+)?$/, "");
+  console.log(`Common letters: ${char}`);
 }
-console.log(commonLetters("houses", "computers"));
+commonLetters("houses", "computers");
