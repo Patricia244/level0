297139f3437596353134ec721c5c553f37d1edc8
Patricia244 <87993886+Patricia244@users.noreply.github.com>
2021-10-13 21:54:16 +0100

    Update task0.9.js

--- task0.9.js
+++ task0.9.js
@@ -1,18 +1,21 @@
 function printVowels(word) {
-  var found = "";
-  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let found = "";
+  let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-  for (var i = 0; i < word.length; i++) {
+  for (let i = 0; i < word.length; i++) {
     if (vowels.includes(word[i])) {
       found += word[i];
       if (word.length - 1 !== i) found += ",";
     }
   }
-  return found
+  let revomeDuplicates = found
     .toLowerCase()
     .split(",")
     .filter(function (item, pos, self) {
       return self.indexOf(item) == pos;
     })
     .join(",");
+  console.log(`Vowels: ${revomeDuplicates}`);
 }
+printVowels("umuzi");
+
